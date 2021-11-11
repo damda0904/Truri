209ee661038d8f21891e41c1675071480e146754
nueeaz <j936596@sookmyung.ac.kr>
2021-11-11 16:22:48 +0900

    add several Pages

---
+++ front/app/src/main/java/com/example/truri/GradeTextPage.java
@@ -0,0 +1,65 @@
+package com.example.truri;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GradeTextPage extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView top_text;
+    private Button submit_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_text_page);
+
+        //뒤로가기 버튼 설정
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitle("");
+
+        //상단 텍스트 '신뢰도' 설정
+        top_text = findViewById(R.id.top_text);
+        SpannableStringBuilder sp = new SpannableStringBuilder("신뢰 가능으로 평가하신 \n 이유를 알려주세요");
+
+        ForegroundColorSpan color = new ForegroundColorSpan(Color.parseColor("#13A6BA"));
+        sp.setSpan(color, 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        top_text.setText(sp);
+
+        //완료버튼 페이지 이동
+        submit_btn = findViewById(R.id.submit_btn);
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ front/app/src/main/java/com/example/truri/Grade_items.java
@@ -0,0 +1,14 @@
+package com.example.truri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grade_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_items);
+    }
+}
