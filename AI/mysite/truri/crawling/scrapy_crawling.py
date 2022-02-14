4983e2f2b1bf3f3c1ee7f52718480b5466e94bf2
damda0904 <suen0904@gmail.com>
2022-02-14 16:18:43 +0900

    Scrapy - naver, google crawling 진행중

--- AI/mysite/truri/crawling/scrapy_crawling.py
+++ AI/mysite/truri/crawling/scrapy_crawling.py
@@ -10,7 +10,7 @@ from twisted.internet import reactor
 #네이버 spider
 class NaverSpider(scrapy.Spider):
     name = 'naver'
-    custom_settings = {'DOWNLOD_DELAY': 1}
+    custom_settings = {'DOWNLOD_DELAY': 10}
 
     def start_requests(self):
         start_url = url[0]
@@ -55,16 +55,10 @@ class NaverSpider(scrapy.Spider):
     def parse_detail(self, response):
         print("Naver : parse_detail!")
 
-        # 카페 iframe
-        if "blog" not in response.url:
-            iframe = response.css('#cafe_main').extract()
-            yield scrapy.Request(url="https:" + iframe[0], callback=self.parse_iframe_cafe)
-        # 블로그 iframe
-        else:
-            iframe = response.css('#mainFrame::attr(src)').extract()
-            yield scrapy.Request(url="https://blog.naver.com/" + iframe[0], callback=self.parse_iframe_blog)
+        iframe = response.css('#mainFrame::attr(src)').extract()
+        yield scrapy.Request(url="https://blog.naver.com/" + iframe[0], callback=self.parse_iframe)
 
-    def parse_iframe_blog(self, response):
+    def parse_iframe(self, response):
         print("Blog : in iframe")
         image = response.css(".se-image-resource").xpath("@src").extract()
 
@@ -84,34 +78,9 @@ class NaverSpider(scrapy.Spider):
         for line in content_data:
             if line != "\u200b" :
                 one_content = one_content + line + " "
-        print("blog------------------------")
         print(one_content)
         content.append(one_content)
 
-    def parse_iframe_cafe(self, response):
-        print("Cafe : in iframe")
-        image = response.css(".se-image-resource").xpath("@src").extract()
-
-        # 이미지가 있을 경우/ 없을 경우
-        if len(image) != 0:
-            preview_image.append(image[0])
-
-            last_images.append([image[len(image) - 2], image[len(image) - 1]])
-        else:
-            preview_image.append("")
-            last_images.append("")
-            last_images.append("")
-
-        # 본문 크롤링
-        content_data = response.css(".se-main-container").xpath(".//p//text()").getall()
-        print(content_data)
-        one_content = ""
-        for line in content_data:
-            if line != "\u200b":
-                one_content = one_content + line + " "
-        print("cafe------------------------")
-        print(one_content)
-        content.append(one_content)
 
 #티스토리 spider
 class TistorySpider(scrapy.Spider):
@@ -207,18 +176,10 @@ def scrapy_crawling(query, page) :
 
     naver_page = (int(page) - 1) * 30 + 1
 
-    # 각각 다른 탭으로 검색했을 때, 블로그는 30개, 카페는 10개 단위
     # 네이버 블로그 검색결과 url
-    # url.append("https://s.search.naver.com/p/blog/search.naver?where=blog&sm=tab_pge&api_type=0&query=" + query +"&rev=44&start=" + str(naver_page) +
-    #            "&dup_remove=1&post_blogurl=&post_blogurl_without=&nso=&nlu_query=%7B%22r_category%22%3A%2229%22%7D&dkey=0&source_query=&nx_search_query="
-    #            + query + "&spq=0&_callback=viewMoreContents")
-
-    # 네이버 블로그, 카페 검색결과 url
-    url.append("https://s.search.naver.com/p/review/search.naver?rev=44&where=view&api_type=0&start="+ str(naver_page) +"&query=" + query +
-               "&nso=&nqx_theme=%7B%22theme%22%3A%7B%22main%22%3A%7B%22name%22%3A%22restaurant_list%22%7D%7D%7D&main_q="+
-               "&mode=normal&q_material=&ac=1&aq=0&spq=0&st_coll=&topic_r_cat=&nx_search_query=&nx_and_query="+
-               "&nx_sub_query=&prank=31&sm=tab_jum&ssc=tab.view.view&ngn_country=KR&lgl_rcode=02135103"+
-               "&fgn_region=&fgn_city=&lgl_lat=37.3614503&lgl_long=127.1115333&_callback=viewMoreContents")
+    url.append("https://s.search.naver.com/p/blog/search.naver?where=blog&sm=tab_pge&api_type=0&query=" + query +"&rev=44&start=" + str(naver_page) +
+               "&dup_remove=1&post_blogurl=&post_blogurl_without=&nso=&nlu_query=%7B%22r_category%22%3A%2229%22%7D&dkey=0&source_query=&nx_search_query="
+               + query + "&spq=0&_callback=viewMoreContents")
 
     Google_page = (int(page) - 1) * 10
 
